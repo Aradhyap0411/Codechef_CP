23ca7be726d256f784011487b955935becc50bde
Coderap <97150200+Aradhyap0411@users.noreply.github.com>
2022-07-10 14:41:08 +0530

    Add files via upload

---
+++ JULY221D/Game_of_Piles_Version_1.cpp
@@ -0,0 +1,75 @@
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <climits>
+#include <vector>
+#include <unordered_set>
+#include <unordered_map>
+using namespace std;
+#define lli long long int
+#define ll long long
+#define pb push_back
+#define mod 1000000007
+#define ite vector<int> :: iterator it
+const int N=10e5+10;
+int main(){
+ios::sync_with_stdio(0);
+cin.tie(0);
+  int t;
+  cin>>t;
+  while(t--)
+  {
+    int n;
+    cin>>n;
+    vector<int>v;
+    int flag=0,sum=0;
+    for(int i=0;i<n;i++){
+        int x;
+        cin>>x;
+        v.pb(x);
+        if(x==1){
+            flag=1;
+        }
+        sum+=x;
+    }
+    if(flag==1){
+        cout<<"CHEF";
+    }
+    else if(sum%2==0){
+        cout<<"CHEFINA";
+    }
+    else{
+        cout<<"CHEF";
+    }
+    cout<<"\n";
+ }
+return 0;
+}
