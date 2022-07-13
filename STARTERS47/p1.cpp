c457f1654d3618b715915a926316da20aa206722
Coderap <97150200+Aradhyap0411@users.noreply.github.com>
2022-07-14 02:44:14 +0530

    Create p1.cpp

---
+++ STARTERS47/p1.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define lli long long int
+#define ll long long
+#define pb push_back
+#define mod 1000000007
+#define ite vector<int> :: iterator it
+#define omap map<int, int>
+#define umap unordered_map<int,int>
+#define oset set<int>
+#define fr(i,n) for(int i=0;i<n;i++)
+#define endl "\n"
+const int N=10e5+10;
+int main(){
+ios::sync_with_stdio(0);
+cin.tie(0);
+  int t;
+  cin>>t;
+  while(t--)
+  {
+    omap m;
+    int n;
+    cin>>n;
+    fr(i,n){
+      int x;
+      
+    }
+    vector<int>v;
+    for(int i=0;i<n;i++){
+      int x;
+      cin>>x;
+      v.pb(x);
+    }
+  }
+return 0;
+}
