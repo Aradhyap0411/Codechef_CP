1aa72e87abc1a76f1ec2e00b6eb1a5eaee1395cf
Coderap <97150200+Aradhyap0411@users.noreply.github.com>
2022-07-14 02:44:41 +0530

    Add files via upload

---
+++ STARTERS47/ccp2.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define lli long long int
+#define ll long long
+#define pb push_back
+#define mod 1000000007
+#define ite vector<int> :: iterator it
+#define endl "\n"
+const int N=10e5+10;
+int main(){
+ios::sync_with_stdio(0);
+cin.tie(0);
+  int t;
+  cin>>t;
+  while(t--)
+  {
+   int n;
+   cin>>n;
+   n=n*60;
+   cout<<n/20<<endl; 
+  }
+return 0;
+}
