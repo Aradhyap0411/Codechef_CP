6872f205a6ce0b572d75042781bea64c2162ec94
Coderap <97150200+Aradhyap0411@users.noreply.github.com>
2022-07-14 02:45:10 +0530

    Add files via upload

---
+++ STARTERS47/ccp3.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define lli long long int
+#define ll long long
+#define pb push_back
+#define mod 1000000007
+#define ite vector<int> :: iterator it
+#define endl "\n"
+const int N=10e5+10;
+int main(){
+ios::sync_with_stdio(0);
+cin.tie(0);
+  int t;
+  cin>>t;
+  while(t--)
+  {
+   int x;
+   cin>>x;
+   int note=x/10;
+   cout<<x-note*10<<endl;
+    
+  }
+return 0;
+}
